e2a7cbb9c4d2bff38d2a4fd45248700199d35bc5
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-09-19 23:04:46 -0700

    feat: trying deeper relation

--- src/app/api/courses/[id]/route.ts
+++ src/app/api/courses/[id]/route.ts
@@ -1,14 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { kv } from "@vercel/kv";
 
-export async function GET(
-    request: Request,
-    { params }: { params: { id: string } }
-) {
-    const id = params.id;
-    const classMembers = await kv.get(id);
-
-    return NextResponse.json(classMembers);
+interface OrganizedData {
+    [key: string]: {
+        [key: string]: Course;
+    }
 }
 
 export interface Lecture {
@@ -26,14 +22,54 @@ export interface Courses {
     email: string;
     courses: Array<Course>;
 }
+
+export interface PostCourses {
+    name: string;
+    email: string;
+    course: Course;
+}
+
+export async function GET(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    const id = params.id;
+    const memberAndCourses = await kv.lrange(id, 0, -1);
+
+    let organizedData = {} as OrganizedData;
+
+    for (const memberAndCourse of memberAndCourses) {
+        const courseDetailofMember = await kv.get(memberAndCourse) as Course;
+        const [member, course] = memberAndCourse.split(":") as string[];
+
+        if (!Object.keys(organizedData).includes(member)) {
+            const courseObj = {
+                [course]: courseDetailofMember,
+            };
+
+            organizedData[member] = courseObj;
+
+        } else {
+            organizedData[member][course] = courseDetailofMember;
+        }
+
+    }
+
+    return NextResponse.json(organizedData);
+}
+
 export async function POST(
     request: NextRequest,
     { params }: { params: { id: string } }
 ) {
-    const json = await request.json() as Courses;
+    const json = await request.json() as PostCourses;
+    const emailAndCourse = `${json.email}:${json.course}`;
 
     try {
-        await kv.set(params.id, json);
+        const inClass = await kv.lrange(params.id, 0, -1);
+        if (!inClass.includes(emailAndCourse))
+            await kv.lpush(params.id, emailAndCourse);
+        await kv.set(emailAndCourse, json);
     } catch (e) {
         return NextResponse.json({ success: false }, { status: 400 })
     }
